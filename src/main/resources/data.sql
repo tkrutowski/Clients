de47cca17f347b138e6c21050c081e05d18e57a1
Tomasz Krutowski <tkrutowski@gmail.com>
2021-02-06 23:00:39 +0100

    connect h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into client_address (id,city, street, zip) values (1,'Pobiedziska', 'Armii Poznań 39','62-010' );
+insert into clients(first_name, name, client_type, address_id) values ('Tomasz', 'Krutowski', 'CUSTOMER',1);
